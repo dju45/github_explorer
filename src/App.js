36eaa98d9d46e7495dbd4ed9154ca9bee06e5cca
dju45 <hostejulien@gmail.com>
2018-12-17 14:24:30 +0100

    add result component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactiveBase, DataSearch } from '@appbaseio/reactivesearch';
+
+import theme from './theme';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="container">
+      <ReactiveBase app="gitxplore-app" credentials="FULjx0b1x:f649ea80-f8b2-41df-9047-8ed3403f8075"
+        type="githubExplorer"
+        theme= {theme}>
+        </ReactiveBase>
+      </section>
     );
   }
 }
